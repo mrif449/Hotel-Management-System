1d00317c51b12f0f0836c6930fd29748b300d202
Samonto Karmaker <samonto0726@gmail.com>
2023-07-04 22:54:17 +0600

    Added basic user authentication

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -2,8 +2,19 @@
 const express = require('express')
 
 //Internal Imports
+const {getLoginPage, login, logout} = require('../controllers/loginController')
+const {logInValidators, validationResultHandler} = require('../middlewares/login/loginValidator')
 
 //Initialize Router
 const router = express.Router()
+
+//Get login page
+router.get('/', getLoginPage)
+
+//Login
+router.post('/', logInValidators, validationResultHandler, login)
+
+//Logout
+router.delete('/', logout)
 
 module.exports = router
