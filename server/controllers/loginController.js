b37f01ea697a1b2b93af22cd41f8fa990f994cbf
Md. Rahadul Islam Fardin <mr.if449@gmail.com>
2023-07-13 18:59:38 +0600

    get all users with pagination

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -11,6 +11,45 @@ const getLoginPage = (req, res, next) => {
     //TODO: Render login page {Should be done in the frontend}
 }
 
+// get users
+const getUsers = async (req, res, next) => {
+
+    try {
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        const options = { password: 0 };
+        const searchRegExp = new RegExp(".*" + search + ".*", "i")
+        const filter = {
+            isAdmin: { $ne: true },
+            $or: [
+                { name: { $regex: searchRegExp } },
+                { email: { $regex: searchRegExp } },
+                { phone: { $regex: searchRegExp } },
+            ]
+        };
+
+        const users = await User.find(filter, options).limit(limit).skip((page - 1) * limit);
+        const count = await User.find(filter).countDocuments();
+        if (!users) throw createError(404, "No user found!");
+        return successResponse(res, {
+            statusCode: 200,
+            message: "Users returned successfully",
+            payload: {
+                users,
+                pagination: {
+                    totalPages: Math.ceil(count / limit),
+                    currentPage: page,
+                    previousPage: page - 1 > 0 ? page - 1 : null,
+                    nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+                },
+            },
+        });
+    } catch (error) {
+        next(error)
+    }
+};
+
 //Login
 const login = async (req, res, next) => {
     try {
@@ -149,6 +188,7 @@ const activateUserAccount = async (req, res, next) => {
 };
 
 module.exports = {
+    getUsers,
     getLoginPage,
     login,
     logout, 
