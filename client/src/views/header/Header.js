6751a3974781bfb585c4ee44f30b6ce6146c6600
Samonto Karmaker <samonto0726@gmail.com>
2023-07-21 20:30:33 +0600

    Added initial header from template

--- client/src/views/header/Header.js
+++ client/src/views/header/Header.js
@@ -2,9 +2,24 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <section class="header">
+
+            <div class="flex">
+                <a href="#home" class="logo">Hotels And Resorts</a>
+                <a href="#availability" class="btn">check availability</a>
+                <div id="menu-btn" class="fas fa-bars"></div>
+            </div>
+
+            <nav class="navbar">
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#reservation">reservation</a>
+                <a href="#gallery">gallery</a>
+                <a href="#contact">contact</a>
+                <a href="#reviews">reviews</a>
+            </nav>
+
+        </section>
     );
 }
 
