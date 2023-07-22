62ec1d1b1d659b536c528c3ed49f182b9aeb73f0
Samonto Karmaker <samonto0726@gmail.com>
2023-07-22 20:38:40 +0600

    Added about from template

--- client/src/views/body/Body.js
+++ client/src/views/body/Body.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeComponent from "./home-page/home";
+import AboutComponent from "./home-page/about";
 
 const Body = () => {
     return (
         <div>
             <HomeComponent />
+            <AboutComponent />
         </div>
     );
 }
