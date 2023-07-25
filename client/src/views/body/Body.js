6f41c6b82391236e2885725dbed32788cad8be00
Samonto Karmaker <samonto0726@gmail.com>
2023-07-25 13:56:09 +0600

    Added services component

--- client/src/views/body/Body.js
+++ client/src/views/body/Body.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeComponent from "./home-page/home";
 import AboutComponent from "./home-page/about";
+import ServicesComponent from "./home-page/services";
 
 const Body = () => {
     return (
         <div>
             <HomeComponent />
             <AboutComponent />
+            <ServicesComponent />
         </div>
     );
 }
