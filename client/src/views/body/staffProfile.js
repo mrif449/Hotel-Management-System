23dd810b657ba433b965a2aba73b44c28893b9eb
Samonto Karmaker <samonto0726@gmail.com>
2023-08-17 23:03:31 +0600

    API call: staff profile

--- client/src/views/body/staffProfile.js
+++ client/src/views/body/staffProfile.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const StaffProfile = () => {
+const StaffProfile = ({User}) => {
     
     const staffData = {
         fullName: "John Doe",
@@ -11,6 +11,22 @@ const StaffProfile = () => {
         profilePic: "https://www.w3schools.com/howto/img_avatar.png"
     }
 
+    const [staff, setStaff] = useState(staffData);
+
+    useEffect(() => {
+        if (User) {
+            fetch(`/api/staff/${User._id}`)
+                .then(res => res.json())
+                .then(data => {
+                    setStaff(data.payload);
+                })
+                .catch(err => {
+                    console.log(err)
+                    setStaff(staffData);
+                });
+        }
+    }, [])
+    
     return (
         <div className="staff-profile">
             <div className="header">
@@ -20,11 +36,11 @@ const StaffProfile = () => {
                 <img src={staffData.profilePic} alt={staffData.fullName} />
             </div>
             <div className="details">
-                <h3>{staffData.fullName}</h3>
-                <p>Email: {staffData.email}</p>
-                <p>Phone Number: {staffData.phoneNumber}</p>
-                <p>NID Number: {staffData.nidNumber}</p>
-                <p>Staff Type: {staffData.staffType}</p>
+                <h3>{staff.fullName}</h3>
+                <p>Email: {staff.email}</p>
+                <p>Phone Number: {staff.phoneNumber}</p>
+                <p>NID Number: {staff.nidNumber}</p>
+                <p>Staff Type: {staff.staffType}</p>
             </div>
         </div>
     );
